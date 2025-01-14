04326f99f6c135df3513e1289353145b65053236
SyedArhumHussain <111390421+SyedArhumHussain@users.noreply.github.com>
2025-01-15 01:45:48 +0500

    Add files via upload

---
+++ IBM Data Science Course.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Data Science Tools and Ecosystem
+
+
+# In[4]:
+
+
+#In this notebook, Data Science Tools and Ecosystem are summarized.
+
+
+# In[6]:
+
+
+#Some of the popular languages that Data Scientists use are
+#Python
+#R
+#SQL
+
+
+# In[ ]:
+
+
+#Some of the commonly used libraries used by Data Scientists include:
+#Pandas
+#NumPy
+#Matplotlib
+
+
+# In[7]:
+
+
+| Data Science Tools |
+|--------------------|
+| Jupyter            |
+| RStudio            |
+| VSCode             |
+
+
+# In[ ]:
+
+
+#Below are a few examples of evaluating arithmetic expressions in Python
+
+
+# In[10]:
+
+
+# This is a simple arithmetic expression to multiply then add integers
+(3*4)+5
+
+
+# In[11]:
+
+
+17-8
+
+
+# In[14]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+hours = 200 / 60
+
+
+# In[15]:
+
+
+## Objectives:
+- List popular languages for Data Science
+- List commonly used libraries for Data Science
+- List open-source tools for Data Science
+- Show examples of arithmetic expressions in Python
+
+
+# In[ ]:
+
+
+## Author
+Syed Arhum Hussain
+
+
+# In[ ]:
+
+
+
+
